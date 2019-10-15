7a30c270a2b8d9bb5a10c95f5546c4c1961a7402
Casey Lang <clang1234@gmail.com>
2019-10-15 17:24:52 -0500

    Restructure

--- main.tf
+++ main.tf
@@ -1,17 +1,7 @@
-variable "string_length" {
-  type = number
-  description = "The length of the random string"
-  default = 8
-}
-
 resource "random_string" "id" {
   keepers = {
       uuid = uuid()
   }
 
   length = var.string_length
-}
-
-output "random_string" {
-  value = random_string.id.result
 }
